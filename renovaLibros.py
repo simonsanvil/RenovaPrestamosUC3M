baa12ef39cc9969b61c03bed93c7b77fde55e0b2
simonsanvil <simonsanvil2012@gmail.com>
2019-10-10 14:10:18 +0200

    It now works!

--- renovaLibros.py
+++ renovaLibros.py
@@ -8,6 +8,53 @@ import subprocess
 
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
+def clickRenovarPrestamos():
+    #Get UC3M credentials:
+    if not os.path.isfile("AulaCredentials.txt"): #If we we dont have the credentials in a file already
+        credentials = getCredentials() # TODO: NUMERO DE ESTUDIANTE y contrasena de aula global
+        NIA = credentials[0]
+        PASS = credentials[1]
+
+    else: #Reading credentials from file in directory if it exists
+        with open("AulaCredentials.txt") as f:
+            lineList = [line.rstrip('\n') for line in open("AulaCredentials.txt")]
+            NIA = lineList[0]
+            PASS = lineList[1]
+
+    #Config selenium driver and webpage
+    driver = webdriver.Chrome(os.path.dirname(os.path.realpath(__file__))+'/chromedriver.exe')
+    URL = 'https://bibliotecas.uc3m.es/primo-explore/account?vid=34UC3M_VU1&lang=en_US&section=overview'
+    driver.get(URL)
+    driver.implicitly_wait(8)
+
+    #Click Login with Aulaglobal button:
+    login_button = driver.find_elements_by_xpath('//*[@id="tab-content-0"]/div/md-content/md-list/md-list-item[1]/div/button')[0]
+    login_button.click()
+
+    #Fill login credentials
+    username = driver.find_element_by_name("adAS_username")
+    password = driver.find_element_by_name("adAS_password")
+
+    username.send_keys(NIA)
+    password.send_keys(PASS)
+    driver.find_element_by_id("submit_ok").click()
+
+    #Click 'Renew' button
+    try:
+        renew_button = driver.find_element_by_xpath('//*[@id="tab-content-1"]/div/div/div/prm-loans-overview/div/div/div/div/button')
+    except Exception as e:
+        print( "NO BOOKS TO RENEW")
+        return e
+    renew_button.click()
+    print('[DONE]')
+
+    listElem =  driver.find_element_by_xpath('//*[@id="tab-content-7"]/div/div/prm-loans/md-list')
+    bookTitles = [item.get_attribute('title') for item in listElem.find_elements_by_tag_name('h3')]
+    bookAuthors = [item.text for item in listElem.find_elements_by_tag_name('h4')]
+    print('THE FOLLOWING BOOKS HAVE BEEN RENEWED:')
+    for i in range(len(bookTitles)):
+        print(str(i+1) + '. ' + bookTitles[i] + ' by ' + bookAuthors[i] + '.')
+
 def getCredentials():
     root = Tk()
     root.iconbitmap('Logo_UC3M.ico')
@@ -40,42 +87,9 @@ def getCredentials():
     root.mainloop()
     return credentialsList
 
-#To install required packages:
-subprocess.call([sys.executable, "-m", "pip", "install", "requirements.txt"]
+def installRequirementsWithPip():
+    #To install required packages:
+    subprocess.call([sys.executable, "-m", "pip", "install", "requirements.txt"])
 
-#Get credentials:
-if not os.path.isfile("AulaCredentials.txt"): #If we we dont have the credentials in a file already
-    credentials = getCredentials() # TODO: NUMERO DE ESTUDIANTE y contrasena de aula global
-    NIA = credentials[0]
-    PASS = credentials[1]
-
-else: #Reading credentials from file in directory if it exists
-    with open("AulaCredentials.txt") as f:
-        lineList = [line.rstrip('\n') for line in open("AulaCredentials.txt")]
-        NIA = lineList[0]
-        PASS = lineList[1]
-
-driver = webdriver.Chrome(os.path.dirname(os.path.realpath(__file__))+'/chromedriver.exe')
-URL = 'https://bibliotecas.uc3m.es/primo-explore/account?vid=34UC3M_VU1&lang=en_US&section=overview'#'https://developer.mozilla.org/en-US/docs/Web/HTML/Element/button'
-driver.get(URL)
-
-#Click Login with Aulaglobal button:
-login_button = driver.find_elements_by_xpath('//*[@id="tab-content-0"]/div/md-content/md-list/md-list-item[1]/div/button')[0]
-login_button.click()
-
-#Fill login credentials
-username = driver.find_element_by_name("adAS_username")
-password = driver.find_element_by_name("adAS_password")
-
-username.send_keys(NIA)
-password.send_keys(PASS)
-
-driver.find_element_by_id("submit_ok").click()
-#
-# driver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + 't')
-# # driver.get(URL)
-# #
-time.sleep(3)
-#Click 'Blocks + messages' button
-blocks_button = driver.find_elements_by_xpath('//*[@id="tab-content-1"]/div/div/div/prm-messages-and-blocks-overview/div/div/md-list/md-list-item[1]/div/button')[0]
-blocks_button.click()
+installRequirementsWithPip()
+clickRenovarPrestamos()
